64be168646d2f975439f5273769aec8ae32b1d04
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-07 03:33:32 +0600

    Update kadanes_algorithm.cpp

--- kadanes_algorithm.cpp
+++ kadanes_algorithm.cpp
@@ -1,1 +1,36 @@
+//Brute force
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int Max_subarray(vector<int>v,int n){
+    int max_sum = INT_MIN;
+
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int sum = 0;
+            for(int k=i;k<=j;k++){
+                sum+=v[k];
+            }
+            max_sum = max(max_sum,sum);
+        }
+    }
+    return max_sum;
+}
+
+int main()
+{
+
+    int n;cin>>n;
+    vector<int>arr(n+1);
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int x = Max_subarray(arr, n);
+    cout<<x;
+    
+}
+
+
+
 
