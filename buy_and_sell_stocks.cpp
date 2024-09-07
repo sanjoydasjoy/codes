0d23156e012ee34b6ce55dfd58cb795f82dc5942
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-07 18:14:22 +0600

    Update buy_and_sell_stocks.cpp

--- buy_and_sell_stocks.cpp
+++ buy_and_sell_stocks.cpp
@@ -1,3 +1,26 @@
+//optimal using kadanes algo
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi = INT_MIN;
+        int mini = prices[0];
+        int n = prices.size();
+
+        for (int i = 1; i < n; i++) {
+
+            int cost = prices[i] - mini;
+            maxi = max(maxi, cost);
+            mini = min(mini, prices[i]);
+        }
+        if (maxi > 0)
+            return maxi;
+        return 0;
+    }
+};
+
+
+
 
 //brute force
 
