012a1d9a8d617e5cd6d7b65f1a554067e0db9534
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-12 05:12:15 +0600

    Update 53_maximum_subarray_kadanes_algorithm.cpp

--- 53_maximum_subarray_kadanes_algorithm.cpp
+++ 53_maximum_subarray_kadanes_algorithm.cpp
@@ -74,3 +74,29 @@ public:
         
     }
 };
+
+
+// print any of the subarray
+
+
+class Solution {
+public:
+	int maxSubArray(vector<int>& nums) {
+
+		int sum =0;
+		int mx = INT_MIN;
+		int start = 0, ansStart = -1, ansEnd = -1;
+		for(int i=0;i<nums.size();i++){
+			if(sum==0) start = i;
+			sum+=nums[i];
+			if(sum>mx){
+				mx=sum;
+				ansStart = start;
+				ansEnd = i;
+			}
+			if(sum<0)sum=0;
+		}
+		return mx;
+
+	}
+};
