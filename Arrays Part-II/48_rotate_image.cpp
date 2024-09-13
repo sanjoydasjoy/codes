10f72f65c63d45b19a458462b4fa39c0442453ce
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-13 15:07:13 +0600

    Update 48_rotate_image.cpp

--- Arrays Part-II/48_rotate_image.cpp
+++ Arrays Part-II/48_rotate_image.cpp
@@ -1,4 +1,4 @@
-// my solution
+// my solution TC O(n*n) SC O(n*n)
 
 class Solution {
 public:
@@ -22,3 +22,74 @@ public:
         
     }
 };
+
+// constant space complexity 
+
+// intution : transpose the matrix and then reverse each row
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+
+
+
+
+
+//
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void tranpose(vector<vector<int>>mat, int n){
+	for(int i=0;i<n-1;i++){
+		for(int j=i;j<n;j++){
+			swap(mat[i][j], mat[j][i]);
+		}
+	}
+	for(int i=0;i<n;i++){
+			reverse(mat[i].begin(),mat[i].end());
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<mat[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main()
+{
+
+	int n;cin>>n
+	vector<vector<int>>mat(n,vector<int>(n));
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cin>>mat[i][j];
+		}
+	}
+
+	tranpose(mat,n);
+	
+}
+
+
+
