4b40f259516469023464c4d7a24101d0648824e7
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-14 07:23:44 +0600

    Update 287_find_the_duplicate_number.cpp

--- Arrays Part-II/287_find_the_duplicate_number.cpp
+++ Arrays Part-II/287_find_the_duplicate_number.cpp
@@ -16,3 +16,24 @@ public:
         return val;
     }
 };
+
+/*
+    Using Hashing / Frequency array
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int val=0;
+        vector<int> frequencyArr(n+1, 0); 
+
+        for(int i=0;i<n;i++){
+            if(frequencyArr[nums[i]]==0){
+                frequencyArr[nums[i]]+=1;
+            }
+            else {val=nums[i];break;}
+        }
+        return val;
+    }
+};
