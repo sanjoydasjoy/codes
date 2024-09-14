30fc996cf410ae2ed7abd8f680616dda08508d4b
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-14 09:08:40 +0600

    Update 287_find_the_duplicate_number.cpp

--- Arrays Part-II/287_find_the_duplicate_number.cpp
+++ Arrays Part-II/287_find_the_duplicate_number.cpp
@@ -38,3 +38,29 @@ public:
         return val;
     }
 };
+
+/*
+    Floyd's Tortoise and Hare (cycle detection) algorithm
+    Linked list cycle method
+    TC:  O(N)  SC: O(1)
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
