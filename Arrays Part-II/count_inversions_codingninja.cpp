2c533721da415e982168b49755c18b3b8ba61ff4
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-16 01:40:23 +0600

    Update count_inversions_codingninja.cpp

--- Arrays Part-II/count_inversions_codingninja.cpp
+++ Arrays Part-II/count_inversions_codingninja.cpp
@@ -1,3 +1,65 @@
+#include <bits/stdc++.h>
+int cnt = 0;
+void merge(vector<int> &nums, int low, int mid, int high) {
+  vector<int> temp;
+  int left = low;
+  int right = mid + 1;
+  while (left <= mid && right <= high) {
+    if (nums[left] <= nums[right]) {
+      temp.push_back(nums[left]);
+      left++;
+    } else {
+      temp.push_back(nums[right]);
+      right++;
+      cnt += (mid - left + 1);
+    }
+  }
+  while (left <= mid) {
+    temp.push_back(nums[left]);
+    left++;
+  }
+  while (right <= high) {
+    temp.push_back(nums[right]);
+    right++;
+  }
+  for (int i = low; i <= high; i++) {
+        nums[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &nums, int low, int high) {
+  if (low >= high)return;
+  
+  int mid = (low + high) / 2;
+  mergeSort(nums, low, mid);
+  mergeSort(nums, mid + 1, high);
+  merge(nums, low, mid, high);
+
+}
+
+long long getInversions(long long *arr, int n) {
+
+  int low = 0;
+  int high = n - 1;
+  vector<int> nums(arr, arr + n);
+
+  mergeSort(nums, low, high);
+
+  return cnt;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // brute force TC: O(n^2) SC: O(1)
 
 #include <bits/stdc++.h> 
