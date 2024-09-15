f8868cff794cfaa4e1834b06ad5449b6f92fabe3
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-15 13:44:17 +0600

    Create count_inversions.cpp

---
+++ Arrays Part-II/count_inversions.cpp
@@ -0,0 +1,13 @@
+/*
+  brute force TC: O(n^2) SC: O(1)
+*/
+#include <bits/stdc++.h> 
+long long getInversions(long long *arr, int n){
+    int cnt=0;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j])cnt++;
+        }
+    }
+    return cnt;
+}
