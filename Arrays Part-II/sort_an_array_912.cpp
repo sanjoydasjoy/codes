e598bf7f7247083efc73e712ff1828846c43df25
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-16 02:29:28 +0600

    Update sort_an_array_912.cpp

--- Arrays Part-II/sort_an_array_912.cpp
+++ Arrays Part-II/sort_an_array_912.cpp
@@ -1,3 +1,56 @@
+// merge sort pseudo_code
+
+merge(arr,low,mid,high){
+	vector<int>temp;
+
+	int left = low;
+	int right = mid+1;
+
+	while(left<=mid && right<=high){
+		if(arr[left]<=arr[right]){
+			temp.push_back(arr[left])
+			left++
+		}
+		else {
+			temp.push_back(arr[right])
+			right++
+		}
+	}
+	while(left<=mid){
+		temp.push_back(arr[left])
+			left++
+	}
+	while(right<=high){
+		temp.push_back(arr[right])
+			right++
+	}
+	for(int i=low;i<=high;i++){
+		arr[i] = temp[i-low]
+	}
+}
+
+
+mergeSort(arr,low,high){
+	if(low>=high)return;
+
+	int mid = (low+high)/2;
+
+	mergeSort(arr,low,mid)
+	mergeSort(arr,mid+1,high)
+	merge(arr,low,mid,high)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // merge sort
 
 class Solution {
