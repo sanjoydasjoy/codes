f3ebc5d8ec00548624de66006c1574df615e945c
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-18 20:52:09 +0600

    Update 229_majority_element_II.cpp

--- Array Part-III/229_majority_element_II.cpp
+++ Array Part-III/229_majority_element_II.cpp
@@ -1,17 +1,26 @@
+/* 
+    hashing soln
+    TC: O(nlogn) // for ordered map
+    SC: O(n) 
+*/
+
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-         int n = nums.size(), mx = -1, p;
+         int n = nums.size();
         map<int, int> mpp;
         vector<int>xp;
 
+        int x = (int)(n/3) + 1;
+
         for (int i = 0; i < n; i++) {
             mpp[nums[i]]++;
-        }
-        for (auto i : mpp) {
-            if (i.second > n/3) {
-                xp.push_back(i.first) ;
+            
+            if (mpp[nums[i]] == x) {  // because elements that appear more than ⌊ n/3 ⌋ times
+                xp.push_back(nums[i]);
             }
+            if(xp.size()==2)break;
         }
         return xp;
     }
