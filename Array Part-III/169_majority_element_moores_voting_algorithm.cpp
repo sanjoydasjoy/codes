695d14f6c5106cd42d94c867194104381f46a290
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-18 17:52:30 +0600

    Update 169_majority_element_moores_voting_algorithm.cpp

--- Array Part-III/169_majority_element_moores_voting_algorithm.cpp
+++ Array Part-III/169_majority_element_moores_voting_algorithm.cpp
@@ -1,3 +1,36 @@
+/* 
+    moore's voting algorithm
+    TC: O(n) 
+      + O(n) [in case the majority element not everytime exists]
+    SC: O(1) 
+
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int el=nums[0],cnt=0,pnt=0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+
+            if(cnt == 0){ cnt=1, el = nums[i];}
+
+            else if(nums[i] == el) cnt++;
+            
+            else cnt--;
+
+        }
+        /*  in case the majority element not everytime exists
+        for(int i=0;i<n;i++){
+            if(nums[i]==el)pnt++;
+        }
+        if(pnt>n/2)return el; */
+        
+        return el;
+        
+    }
+};
 
 /* 
     hashing solution - better soln
