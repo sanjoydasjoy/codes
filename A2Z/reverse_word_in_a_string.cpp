38cfe1d470ebdcb73f68a39ba1e8a92b489fb45c
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-20 04:10:25 +0600

    Update reverse_word_in_a_string.cpp

--- A2Z/reverse_word_in_a_string.cpp
+++ A2Z/reverse_word_in_a_string.cpp
@@ -1,1 +1,25 @@
+// my O(n) soln
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string>ss;
+        string q,p;
 
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' ')q+=s[i];
+            else{
+                if(q.size()>0){ss.push_back(q);q.clear();}
+                else continue;
+            }
+        }
+        if(q.size() > 0) {  
+            ss.push_back(q);
+        }
+        for(int i=ss.size()-1;i>=0;i--){
+            p+=ss[i];
+            if(i!=0) p += " ";
+        }
+        return p;
+
+    }
+};
