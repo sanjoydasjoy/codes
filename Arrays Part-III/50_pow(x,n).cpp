f9224e7649976be2301985a2b933383cd4001c49
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-20 03:44:24 +0600

    Create 50_pow(x,n).cpp

---
+++ Arrays Part-III/50_pow(x,n).cpp
@@ -0,0 +1,46 @@
+//optimal
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        double ans = 1;
+        long long p = n;
+        if(n<0)p=-p;
+
+
+        while(p>0){
+            if(p%2==1){
+                ans *= x;
+                p = p-1;
+            }
+            else {
+                x *= x;
+                p /= 2;
+            }
+        }
+        
+        
+        if(n>0)return ans;
+        else return (1/ans);
+        
+    }
+};
+
+
+//brute force
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        double ans = 1;
+
+        if(n>0) for(double i=1;i<=n;i++)ans*=x;
+        
+        else for(double i=1;i<=(-n);i++)ans*=x;
+        
+        
+        if(n>0)return ans;
+        else return (1/ans);
+        
+    }
+};
