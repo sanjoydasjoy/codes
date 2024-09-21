cd93ff8c4c7be36bee6f58b50dedcca9860d7444
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-22 00:20:08 +0600

    Update 15_3_sum.cpp

--- A2Z/15_3_sum.cpp
+++ A2Z/15_3_sum.cpp
@@ -1,3 +1,30 @@
+// HashMap
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        set< vector<int> >st;
+        for(int i=0;i<n;i++){
+            set<int>HashSet;
+            for(int j=i+1;j<n;j++){
+                int third = -(nums[i]+nums[j]);
+                if(HashSet.find(third) != HashSet.end()){
+                    vector<int>temp = {nums[i],nums[j],third};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+                HashSet.insert(nums[j]);
+            }
+        }
+        vector< vector<int> > ans (st.begin(),st.end());
+        return ans;
+    }
+};
+
+
+
+
 // brute force TC: O(n^3)
 
 class Solution {
