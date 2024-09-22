af38540c2d7642ab26c925c6e85e9f92d01cdf38
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-22 17:28:11 +0600

    Update 18_4_sum.cpp

--- Arrays Part-IV/18_4_sum.cpp
+++ Arrays Part-IV/18_4_sum.cpp
@@ -1,1 +1,27 @@
+//brute force TC: O(n^4) SC: O(number of quads)*2 for set and ans
 
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>>st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    for(int l=k+1;l<n;l++){
+                        long long sum=nums[i]+nums[j];
+                        sum+=nums[k];
+                        sum+=nums[l];
+                        if(sum==target){
+                            vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+                            sort(temp.begin(),temp.end());
+                            st.insert(temp);
+                        }
+                    }
+                }
+            }
+        }
+        vector<vector<int>>ans = {st.begin(),st.end()};
+        return ans;
+    }
+};
