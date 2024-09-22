eb8e956cb22955883eea2176c01a9769f1ee1772
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-22 16:25:05 +0600

    Update 15_3_sum.cpp

--- Arrays Part-IV/15_3_sum.cpp
+++ Arrays Part-IV/15_3_sum.cpp
@@ -1,3 +1,62 @@
+// optimal soln without set data structure
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0)k--;
+                else if(sum<0)j++;
+                else{
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+// HashMap
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        set< vector<int> >st;
+        for(int i=0;i<n;i++){
+            set<int>HashSet;
+            for(int j=i+1;j<n;j++){
+                int third = -(nums[i]+nums[j]);
+                if(HashSet.find(third) != HashSet.end()){
+                    vector<int>temp = {nums[i],nums[j],third};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+                HashSet.insert(nums[j]);
+            }
+        }
+        vector< vector<int> > ans (st.begin(),st.end());
+        return ans;
+    }
+};
+
+
+
+
 // brute force TC: O(n^3)
 
 class Solution {
