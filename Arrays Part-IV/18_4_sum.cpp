bae472983ce95820e793be9fb255bc17ab6d7ba5
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-22 18:22:59 +0600

    Update 18_4_sum.cpp

--- Arrays Part-IV/18_4_sum.cpp
+++ Arrays Part-IV/18_4_sum.cpp
@@ -1,3 +1,34 @@
+// better soln 
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>>st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                set<long long>HashSet;
+                for(int k=j+1;k<n;k++){
+                        long long sum=nums[i]+nums[j];
+                        sum+=nums[k];
+                        int fourth = target - sum;
+                        if(HashSet.find(fourth) != HashSet.end()){
+                            vector<int>temp={nums[i],nums[j],nums[k],fourth};
+                            sort(temp.begin(),temp.end());
+                            st.insert(temp);
+                        }
+                    HashSet.insert(nums[k]);
+                }
+            }
+        }
+        vector<vector<int>>ans = {st.begin(),st.end()};
+        return ans;
+    }
+};
+
+
+
+
 //brute force TC: O(n^4) SC: O(number of quads)*2 for set and ans
 
 class Solution {
