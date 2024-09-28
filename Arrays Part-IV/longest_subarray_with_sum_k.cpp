806fa65cc819e1c1a99a33e9d99cac7d8a90aaec
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-09-28 20:04:06 +0600

    Update longest_subarray_with_sum_k.cpp

--- Arrays Part-IV/longest_subarray_with_sum_k.cpp
+++ Arrays Part-IV/longest_subarray_with_sum_k.cpp
@@ -1,1 +1,13 @@
-
+// brute force O(n^2)
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int len=0;
+    int n = a.size();
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum+=a[j];
+            if(sum==k)len=max(len,j-i+1);
+        }
+    }
+    return len;
+}
