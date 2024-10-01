ff762e5c1062829b8e0a0378d570c1b78e87824b
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-01 13:03:51 +0600

    Update count_the_number_of_subarrays_with_given_xor_K.cpp

--- Arrays Part-IV/count_the_number_of_subarrays_with_given_xor_K.cpp
+++ Arrays Part-IV/count_the_number_of_subarrays_with_given_xor_K.cpp
@@ -1,3 +1,22 @@
+//optimal appraoch TC: O(nlogn) SC: O(n)
+
+#include<bits/stdc++.h>
+int subarraysWithSumK(vector < int > a, int b) {
+    int cnt=0,x=0;
+    int xr=0;
+    map<int,int>mpp;
+    mpp[xr]++;
+    for(int i=0;i<a.size();i++){
+        xr^=a[i];
+        x=xr^b;
+        cnt+=mpp[x];
+        mpp[xr]++;
+    }
+    return cnt;
+}
+
+
+
 //brute force TC: O(n^2) SC: O(1)
 
 int subarraysWithSumK(vector < int > a, int b) {
