76e4ec695c8faa17532162626efd3c4d16406864
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-07 19:05:09 +0600

    Update Traffic Light.cpp

--- cp-31/1k grand/Traffic Light.cpp
+++ cp-31/1k grand/Traffic Light.cpp
@@ -1,3 +1,5 @@
+//optimal
+
 #include <bits/stdc++.h>
 using namespace std;
 #define endl "\n"
@@ -18,68 +20,81 @@ vector<int>prime;
 string s1,t1;
 
 void solve();
-int bin_search(vector<int>v,int num,int len){
-	int first=0,end=len-1;
-	int mid=(first+end)/2;
-	while(first<=end){
-		if(v[mid]==num)return mid+1;
-		else if(v[mid]>num)end=mid-1;
-		else first=mid+1;
-		mid=(first+end)/2;
-	}
-	return 0;
-}
-void sieve(int n){
-	vector<bool>isPrime(n+1,true);
-	int ans=0;
-	for(int i=2;i<=sqrt(n);i++){
-		if(isPrime[i]){
-			for(int j=i*i;j<=n;j=j+i)
-				isPrime[j]=false;
-		}
-	} 
-	for(int i=2;i<=n;i++)
-		if(isPrime[i])
-			prime.push_back(i);
-	}
-	bool comp(pair<int,int>x,pair<int,int>y){
-		return x.second<y.second;
-	}
-	int dp1(int i,int j){
-		if(i==s1.size() or j==t1.size())return 0;
 
-		if(dp[i][j]!=-1)return dp[i][j]; 
-		int res=0;
-		if(s1[i]==t1[j]){
-			res = 1+dp1(i+1,j+1);
-			mark[i][j]=1;
+	int32_t main()
+	{
+		ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+		int t=1;
+		cin>>t;
+		while(t--){
+			solve();
 		}
-		else{
-			int tmp1=dp1(i+1,j);
-			int tmp2=dp1(i,j+1);
-			if(tmp1>tmp2){
-				mark[i][j]=2;
+
+		return 0;
+	}
+	void solve()
+	{
+		int n;
+		char c;
+		cin>>n>>c;
+		string s;cin>>s;
+		int mx=0,x=0;
+		s+=s;
+		for(int i=0;i<s.size();i++){
+			if(s[i]==c){
+				x=0;
+				while(s[i]!='g' && i<s.size()){
+					x++;
+					i++;
+				}
+				if(x>mx)mx=x;
 			}
-			else mark[i][j]=3;
-
-			res= max(tmp1,tmp2);
 		}
-		return dp[i][j]=res;
-
+		if(c=='g')cout<<0<<endl;
+		else cout<<mx<<endl;
 	}
-	void print_way(int i,int j){
-		if(i==s1.size() or j==t1.size())return;
 
-		if(mark[i][j]==1){
-			cout<<s1[i];
-			print_way(i+1,j+1);
-		}
-		else if(mark[i][j]==2){
-			print_way(i+1,j);
-		}
-		else print_way(i,j+1);
 
-	}
+/*
+6
+5 r
+rggry
+1 g
+g
+3 r
+rrg
+5 y
+yrrgy
+7 r
+rgrgyrg
+9 y
+rrrgyyygy
+
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define all(v) v.begin(),v.end()
+#define ho cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+#define show(x) for(auto i:x)cout<<i<<" ";cout<<endl;
+const int inf = 1e9;
+const int N = 1e6 +7;
+
+#define alliswell Things will get better
+
+
+int dp[3005][3005];
+int mark[3005][3005];
+vector<int>prime;
+string s1,t1;
+
+void solve();
+
 	int32_t main()
 	{
 		ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
