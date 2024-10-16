6b13d2ff6e0c8f6b981bd53b868f8d679dd21c2b
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-16 15:01:17 +0600

    Update 31.cpp

--- Sleek/31.cpp
+++ Sleek/31.cpp
@@ -1,5 +1,5 @@
 /*
-31-marathon.cpp
+31-marathon
 Initially you have H amount of energy and D distance to travel. You may travel with any of the given 5 speeds. But you may only travel in units of 1 km. For each km distance traveled, you will spend corresponding amount of energy. e.g. the given speeds are:
 Cost of traveling 1 km: [4, 5, 2, 3, 6]
 Time taken to travel 1 km: [200, 210, 230, 235, 215]
