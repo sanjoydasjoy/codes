731752062c6da473e18cd0fa7424352dbf420b90
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-20 02:57:53 +0600

    Update Detect Cycle in Undirected Graph_DFS.cpp

--- A2Z/Graphs/Lec_2/Detect Cycle in Undirected Graph_DFS.cpp
+++ A2Z/Graphs/Lec_2/Detect Cycle in Undirected Graph_DFS.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+  private: 
+  bool detect_cycle_dfs(vector<vector<int>>& adj, int vis[], int node, int parent){
+        vis[node] = 1;
+        for(auto i : adj[node]){
+            if(!vis[i]){
+                if(detect_cycle_dfs(adj,vis,i,node)) return true;
+            }
+            else if(parent != i) return true;
+        }
+        return false;
+  }
+  public:
+    bool isCycle(vector<vector<int>>& adj) {
+        int n = adj.size();
+        int vis[n] = {0};
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                if(detect_cycle_dfs(adj,vis,i,-1)) return true;
+            }
+        }
+        return false;
+    }
+};
