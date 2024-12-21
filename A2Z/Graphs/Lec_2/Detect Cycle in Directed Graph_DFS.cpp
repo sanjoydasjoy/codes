5c324eda2e112e0e91da31cdc7d7d7cd109f34dd
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-22 00:30:47 +0600

    Create Detect Cycle in Directed Graph_DFS.cpp

---
+++ A2Z/Graphs/Lec_2/Detect Cycle in Directed Graph_DFS.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  private:
+  bool dfs(vector<vector<int>> adj, vector<int>&vis, vector<int>&path_tracker, int node){
+      vis[node] = 1;
+      path_tracker[node] = 1;
+      for(auto it : adj[node]){
+          if(!vis[it]){
+              if(dfs(adj, vis, path_tracker, it)) return true;
+          }
+          else if(path_tracker[it] == 1) return true;
+      }
+      path_tracker[node] = 0;
+      return false;
+  }
+  public:
+    
+    bool isCyclic(int V, vector<vector<int>> adj) {
+        int n = adj.size();
+        vector<int>vis(n,0);
+        vector<int>path_tracker(n,0);
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                if(dfs(adj, vis, path_tracker, i))return true;
+            }
+        }
+        return false;
+    }
+};
