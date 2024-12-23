2c876c7781ca6e7d926296d1e6b2622ec9fe1c96
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-23 21:22:35 +0600

    Update Bipartite Graph.cpp

--- A2Z/Graphs/Lec_2/Bipartite Graph.cpp
+++ A2Z/Graphs/Lec_2/Bipartite Graph.cpp
@@ -1,3 +1,9 @@
+1. Most graphs are bipartite, especially if you randomly check graphs or trees (which are always bipartite).
+2. False is the outlier because it represents an exceptional case where the graph violates bipartite conditions.
+3. The function is designed to return true for the majority (bipartite) cases and false only when the outlier condition (not bipartite) is detected.
+4. The default assumption is that graphs are bipartite. The algorithm focuses on identifying the rare case where it's not bipartite.
+
+    
 class Solution {
 private:
     bool dfs(vector<vector<int>>& graph, int col, vector<int>& color, int node){
