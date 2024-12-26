94dc06968312ca3db2143c809e64f03833ccc5b7
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-26 22:42:44 +0600

    Update Number of Distinct Islands_BFS.cpp

--- Graph/Number of Distinct Islands_BFS.cpp
+++ Graph/Number of Distinct Islands_BFS.cpp
@@ -1,3 +1,5 @@
+question link: https://www.geeksforgeeks.org/problems/find-the-number-of-islands/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find_the_number_of_islands
+
 class Solution {
   private:
     void bfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int row, int col){
