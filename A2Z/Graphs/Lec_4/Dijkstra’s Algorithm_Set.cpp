2464d592b87e3a5658cd71c56cdb5751706421b4
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-28 15:58:55 +0600

    Update Dijkstra’s Algorithm_Set.cpp

--- A2Z/Graphs/Lec_4/Dijkstra’s Algorithm_Set.cpp
+++ A2Z/Graphs/Lec_4/Dijkstra’s Algorithm_Set.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+  public:
+    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+        int n = adj.size();
+        set<pair<int,int>>st;
+        vector<int>dist(n,1e9);
+        dist[src] = 0;
+        st.insert({0,src});
+        
+        while(!st.empty()){
+            auto it = *(st.begin());
+            int dis = it.first;
+            int node = it.second;
+            st.erase(it);
+            
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int adjWeight = it.second;
+                
+                if(adjWeight + dis < dist[adjNode]){
+                    dist[adjNode] = adjWeight + dis;
+                    st.insert({dist[adjNode], adjNode});
+                }
+            }
+        }
+        
+        return dist;
+        
+    }
+};
