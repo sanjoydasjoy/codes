24ae8aa185a02bfd2dc8bd3163150dbb761fc74c
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-31 23:51:28 +0600

    Update Course Schedule - I.cpp

--- A2Z/Graphs/Lec_3/Course Schedule - I.cpp
+++ A2Z/Graphs/Lec_3/Course Schedule - I.cpp
@@ -1,1 +1,36 @@
+question link: 
+https://www.geeksforgeeks.org/problems/prerequisite-tasks/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=prerequisite-tasks
 
+class Solution {
+  public:
+    bool isPossible(int N, int P, vector<pair<int, int> >& prerequisites) {
+        vector<vector<int>>adj(N);
+        for(auto it : prerequisites){
+            adj[it.first].push_back(it.second);
+        }
+        vector<int>indegree(N);
+        for(int i=0;i<N;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(!indegree[it])q.push(it);
+            }
+        }
+        if(topo.size()==N) return true;
+        return false;
+    }
+};
