083d4c9092891f6cd70fa969d77b5043b81a59a0
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-31 22:08:49 +0600

    Update Course Schedule - II.cpp

--- A2Z/Graphs/Lec_4/Course Schedule - II.cpp
+++ A2Z/Graphs/Lec_4/Course Schedule - II.cpp
@@ -1,1 +1,36 @@
+class Solution
+{
+  public:
+    vector<int> findOrder(int V, int m, vector<vector<int>> prerequisites) 
+    {
+        vector<vector<int>>adj(V);
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>indegree(V);
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(!indegree[it])q.push(it);
+            }
+        }
+        if(topo.size()==V) return topo;
+        return {};
 
+    }
+};
