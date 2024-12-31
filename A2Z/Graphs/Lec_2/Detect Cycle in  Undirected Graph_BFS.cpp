75415eb54d666dd0e234cc98444b0448368672b9
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-12-31 21:17:14 +0600

    Update Detect Cycle in  Undirected Graph_BFS.cpp

--- A2Z/Graphs/Lec_2/Detect Cycle in  Undirected Graph_BFS.cpp
+++ A2Z/Graphs/Lec_2/Detect Cycle in  Undirected Graph_BFS.cpp
@@ -1,3 +1,6 @@
+question link:
+https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-an-undirected-graph
+
 class Solution {
   private:
   bool detect_cycle_bfs(vector<vector<int>>& adj, int vis[], int src){
