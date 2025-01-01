6550f9320c605f378c8c48cad6d1abd7f2350ca5
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-01 21:26:03 +0600

    Update Prim's Algorithm.cpp

--- A2Z/Graphs/Lec 5: MST & Disjoint Set Problems/Prim's Algorithm.cpp
+++ A2Z/Graphs/Lec 5: MST & Disjoint Set Problems/Prim's Algorithm.cpp
@@ -31,3 +31,76 @@ class Solution {
         return sum;
     }
 };
+
+
+// for ide
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
+
+        vector<int> vis(V, 0);
+        int sum = 0;
+
+        while (!pq.empty()) {
+            int weight = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if (vis[node]) continue;
+            vis[node] = 1;
+            sum += weight;
+
+            for (auto it : adj[node]) {
+                int adjNode = it[0];
+                int edgeW = it[1];
+                if (!vis[adjNode]) {
+                    pq.push({edgeW, adjNode});
+                }
+            }
+        }
+
+        return sum;
+    }
+};
+
+void addEdge(vector<vector<int>> adj[], int u, int v, int w) {
+    adj[u].push_back({v, w});
+    adj[v].push_back({u, w});
+}
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<int>> adj[V];
+
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        addEdge(adj, u, v, w);
+    }
+
+    Solution obj;
+    cout << obj.spanningTree(V, adj) << endl;
+
+    return 0;
+}
+
+// and test cases
+
+5 6
+0 1 2
+0 3 6
+1 2 3
+1 3 8
+1 4 5
+2 4 7
+
