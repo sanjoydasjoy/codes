3111d08a376bb017ce143ffda7ba1cf1031c2801
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-01 12:54:56 +0600

    Update Alien Dictionary.cpp

--- A2Z/Graphs/Lec_3/Alien Dictionary.cpp
+++ A2Z/Graphs/Lec_3/Alien Dictionary.cpp
@@ -1,6 +1,22 @@
-
+question link:
+https://www.geeksforgeeks.org/problems/alien-dictionary/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=alien-dictionary
 
 // The problem revolves around determining a valid order of characters, which is a classic use case for topological sorting in directed acyclic graphs (DAG).
+// You build a directed graph where: each node represents a character in the alphabet. A directed edge u -> v means u comes before v in the alien language.
+// Constructing the graph requires comparing consecutive words in the dictionary and finding the first mismatching character.
+// and then just bfs (kahns algoritm)
+
+
+// points to be noted: 
+// i initially wanted to use char in the adjacency list but switched to int for simplicity. 
+// This is a key design decision in problems where data can be represented more efficiently.
+// if you give vector<vector<int>>adj, it will give segmentation error. it should always be vector<vector<int>>adj(k)
+// Mapping characters to indices (e.g., 'a' -> 0, 'b' -> 1) by adj[s1[j] - 'a'].push_back(s2[j] - 'a');
+// Always initialize the graph with the expected size (vector<vector<int>>(k) for k nodes).
+// 
+
+
+
 
 
 class Solution {
