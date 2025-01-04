3f834fd726f52d3cc1752aa9ee6acda7ae64e8e5
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-04 22:41:00 +0600

    Update Find eventual safe states.cpp

--- A2Z/Graphs/Lec 3: Topo Sort and Problems/Find eventual safe states.cpp
+++ A2Z/Graphs/Lec 3: Topo Sort and Problems/Find eventual safe states.cpp
@@ -1,1 +1,40 @@
+question link: 
+https://www.geeksforgeeks.org/problems/eventual-safe-states/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=eventual-safe-states
 
+class Solution {
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        vector<vector<int>>adjj(V);
+        vector<int>indegree(V,0);
+        for(int i=0;i<V;i++){
+            // this is i -> it
+            // we need it -> i
+            for(auto it:adj[i]){
+                adjj[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(!indegree[i]){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adjj[node]){
+                indegree[it]--;
+                if(!indegree[it]){
+                q.push(it);
+                }
+            }
+        }
+        sort(topo.begin(),topo.end());
+        return topo;
+        
+        
+    }
+};
