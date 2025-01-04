ef0338e1ad89f1ee6776f62c5e122644993e59a3
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-04 22:39:34 +0600

    Update Course Schedule - II.cpp

--- A2Z/Graphs/Lec 3: Topo Sort and Problems/Course Schedule - II.cpp
+++ A2Z/Graphs/Lec 3: Topo Sort and Problems/Course Schedule - II.cpp
@@ -1,1 +1,37 @@
+question link: 
+https://www.geeksforgeeks.org/problems/course-schedule/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=course-schedule
 
+class Solution{
+  public:
+    vector<int> findOrder(int V, int m, vector<vector<int>> prerequisites) {
+        vector<vector<int>>adj(V);
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>indegree(V);
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(!indegree[it])q.push(it);
+            }
+        }
+        if(topo.size()==V) return topo;
+        return {};
+
+    }
+};
