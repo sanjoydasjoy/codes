3c0d8fd307856ad17e25f6a12e2cf779c143a4e5
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-04 22:41:38 +0600

    Update Kahn's Algorithm.cpp

--- A2Z/Graphs/Lec 3: Topo Sort and Problems/Kahn's Algorithm.cpp
+++ A2Z/Graphs/Lec 3: Topo Sort and Problems/Kahn's Algorithm.cpp
@@ -1,1 +1,33 @@
+question link: 
+https://www.geeksforgeeks.org/problems/topological-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=topological-sort
 
+class Solution {
+  public:
+    vector<int> topologicalSort(vector<vector<int>>& adj) {
+        int n =  adj.size();
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        return ans;
+    }
+};
