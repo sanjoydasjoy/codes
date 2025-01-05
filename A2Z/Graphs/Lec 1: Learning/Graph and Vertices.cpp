5c82635edd7fefa291a83394caf5eeb2c2785277
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-05 23:20:16 +0600

    Update Graph and Vertices.cpp

--- A2Z/Graphs/Lec 1: Learning/Graph and Vertices.cpp
+++ A2Z/Graphs/Lec 1: Learning/Graph and Vertices.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+  public:
+    long long count(int n) {
+        long long x = n*(n-1);
+        long long y = x/2;
+        long long z = pow(2,y);
+        return z;
+    }
+};
