9ca05f3692fbb8c2400bf7d2c81cf72af231231c
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-09 23:55:21 +0600

    Update Number of Distinct Islands_BFS_GFG.cpp

--- Graph Part-I/Number of Distinct Islands_BFS_GFG.cpp
+++ Graph Part-I/Number of Distinct Islands_BFS_GFG.cpp
@@ -1,4 +1,5 @@
-question link: https://www.geeksforgeeks.org/problems/find-the-number-of-islands/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find_the_number_of_islands
+question link: 
+https://www.geeksforgeeks.org/problems/find-the-number-of-islands/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find_the_number_of_islands
 
 class Solution {
   private:
