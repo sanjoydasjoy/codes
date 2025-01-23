a3cd227d0f91f9580af4f4a8070801e830d1b6ae
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-23 09:56:11 +0600

    Create run.cpp

---
+++ zacademy/os/codes/run.cpp
@@ -0,0 +1,35 @@
+cd D:\VSCode\OS
+
+1. g++ fcfs.cpp -o f
+
+2. g++ sjf.cpp -o s
+
+3. g++ Producer_consumer_using_bounded_buffer_scheme.cpp -o p
+
+4. g++ bsemaphore.cpp -o b
+
+5. g++ Producer_Consumer_Using_Semaphore.cpp -o r
+
+6. g++ Semaphore_with_no_busy_waiting.cpp -o t   
+
+7. g++ Dining_philosophers.cpp -o k
+
+8. g++ Bankers_Algorithm.cpp -o a
+
+
+1.
+5
+1 0 4
+2 1 3
+3 2 1
+4 3 2
+5 4 6
+
+
+2.
+5
+1 0 4
+2 2 3
+3 4 1
+4 5 2
+5 6 6
