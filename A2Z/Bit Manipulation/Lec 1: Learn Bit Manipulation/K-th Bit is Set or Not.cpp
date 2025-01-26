6c91fd20e30674fb587000e1f916fa6676e14ef9
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-27 02:54:42 +0600

    Update K-th Bit is Set or Not.cpp

--- A2Z/Bit Manipulation/Lec 1: Learn Bit Manipulation/K-th Bit is Set or Not.cpp
+++ A2Z/Bit Manipulation/Lec 1: Learn Bit Manipulation/K-th Bit is Set or Not.cpp
@@ -1,1 +1,10 @@
+question link:
+https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
 
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        if (((n >> k) & 1) == 1) return true;
+        return false;
+    }
+};
