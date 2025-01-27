45fe2dbe221c64ee124a2ad60a8d4fa66239f0b3
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-28 02:20:54 +0600

    Update Count total set bits.cpp

--- A2Z/Bit Manipulation/Lec 1: Learn Bit Manipulation/Count total set bits.cpp
+++ A2Z/Bit Manipulation/Lec 1: Learn Bit Manipulation/Count total set bits.cpp
@@ -1,7 +1,7 @@
 question link:
 https://www.geeksforgeeks.org/problems/count-total-set-bits-1587115620/1
 
-brute force:
+// brute force:
 
 class Solution{
     public:
@@ -18,5 +18,38 @@ class Solution{
         }
         return cnt;
         
+    }
+};
+
+// better but this still gives time limit:
+
+class Solution{
+    public:
+    int countSetBits(int n)
+    {
+        int cnt =0;
+        for (int i = 1; i <= n; i++){
+          int num = i;
+          while(num != 0){
+            num = num&(num-1);
+            cnt++;
+          }
+        }
+        return cnt;
+    }
+};
+
+
+// using builtin function:
+
+class Solution{
+    public:
+    int countSetBits(int n)
+    {
+        int cnt =0;
+        for (int i = 1; i <= n; i++){
+          cnt+=__builtin_popcount(i);
+        }
+        return cnt;
     }
 };
