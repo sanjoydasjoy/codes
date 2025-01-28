f7b58f9eed97a0c73992735e0016376f14ca11c2
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-28 23:14:49 +0600

    Update Divide Two Integers.cpp

--- A2Z/Bit Manipulation/Lec 1: Learn Bit Manipulation/Divide Two Integers.cpp
+++ A2Z/Bit Manipulation/Lec 1: Learn Bit Manipulation/Divide Two Integers.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long n = abs((long)dividend);
+        long d = abs((long)divisor);
 
+        bool sign = true;
+        if (dividend >= 0 and divisor < 0)
+            sign = false;
+        if (dividend < 0 and divisor >= 0)
+            sign = false;
+
+        long cnt = 0;
+        while (n >= d) {
+            int i = 0, cum = 0;
+            while (n >= d << i) {
+                i++;
+            }
+            cnt += 1 << (i - 1);
+            n -= d << (i - 1);
+        }
+        if (cnt == (1 << 31) && sign) {
+            return INT_MAX;
+        }
+
+        if (cnt == (1 << 31) && !sign) {
+            return INT_MIN;
+        }
+        return sign ? cnt : -cnt;
+    }
+};
