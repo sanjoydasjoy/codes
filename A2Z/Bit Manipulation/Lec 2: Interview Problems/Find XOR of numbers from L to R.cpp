6b03052488e52c683aa75db582c77aece4d82995
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-29 14:00:05 +0600

    Update Find XOR of numbers from L to R.cpp

--- A2Z/Bit Manipulation/Lec 2: Interview Problems/Find XOR of numbers from L to R.cpp
+++ A2Z/Bit Manipulation/Lec 2: Interview Problems/Find XOR of numbers from L to R.cpp
@@ -24,15 +24,16 @@ class Solution {
 // correct
 
 class Solution {
+private:
+    int xorUpTo(int n) {
+        if (n % 4 == 0) return n;
+        if (n % 4 == 1) return 1;
+        if (n % 4 == 2) return n + 1;
+        return 0; 
+    }
+
 public:
     int findXOR(int l, int r) {
-        int xorUpTo(int n) {
-            if (n % 4 == 0) return n;
-            if (n % 4 == 1) return 1;
-            if (n % 4 == 2) return n + 1;
-            return 0; // n % 4 == 3
-        }
-
         return xorUpTo(r) ^ xorUpTo(l - 1);
     }
 };
