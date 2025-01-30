0adcd7435e606c13b29fd8dfc388b3f19cfe7e9e
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-30 23:56:23 +0600

    Update Reverse Integer.cpp

--- A2Z/Basics/Lec 4: Maths/Reverse Integer.cpp
+++ A2Z/Basics/Lec 4: Maths/Reverse Integer.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int reverse(int x) {
+        long reverseDigit = 0;
+        while(x!=0){
+            long lastDigit = x%10;
+            x/=10;
+            reverseDigit = reverseDigit*10 + lastDigit;
+        }
+        if (reverseDigit < INT_MIN || reverseDigit > INT_MAX) return 0;
+        return reverseDigit;
+    }
+};
