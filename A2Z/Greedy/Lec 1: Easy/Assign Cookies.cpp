11d4af8539b9a9e915c72b1541eb6b6fdaa21ff3
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-31 16:21:58 +0600

    Update Assign Cookies.cpp

--- A2Z/Greedy/Lec 1: Easy/Assign Cookies.cpp
+++ A2Z/Greedy/Lec 1: Easy/Assign Cookies.cpp
@@ -1,1 +1,19 @@
+question link:
+https://leetcode.com/problems/assign-cookies/
 
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int i = 0, j = 0, cnt = 0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        while(j<s.size() and i<g.size()){
+            if(s[j] >= g[i]){
+                cnt++;
+                i++;
+            }
+            j++;
+        }
+        return cnt;
+    }
+};
