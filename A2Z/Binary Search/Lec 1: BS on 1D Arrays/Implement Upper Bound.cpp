1ac21a58241111c3db5e12678582857f48c7b8af
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-31 23:44:19 +0600

    Update Implement Upper Bound.cpp

--- A2Z/Binary Search/Lec 1: BS on 1D Arrays/Implement Upper Bound.cpp
+++ A2Z/Binary Search/Lec 1: BS on 1D Arrays/Implement Upper Bound.cpp
@@ -1,1 +1,26 @@
 question link:
+https://www.naukri.com/code360/problems/implement-upper-bound_8165383?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries
+
+int upperBound(vector<int> &arr, int x, int n){
+	
+	int left = 0, right = arr.size() - 1;
+	int ans = n;
+	while(left<=right){
+		int mid = left + ((right - left) >> 1);
+		if(arr[mid] > x){
+			ans = mid;
+			right = mid - 1;
+		}
+		else{
+			left = mid + 1;
+		}
+	}
+	return ans;	
+}
+
+// using built in function
+
+int upperBound(vector<int> &arr, int x, int n){
+	int ub = upper_bound(arr.begin(),arr.end(),x) - arr.begin();
+	return ub;
+}
