bbe3f3545af02a1c7fe6981a609d12628fd3d84f
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-02-01 02:32:02 +0600

    Update Floor in a Sorted Array.cpp

--- A2Z/Binary Search/Lec 1: BS on 1D Arrays/Floor in a Sorted Array.cpp
+++ A2Z/Binary Search/Lec 1: BS on 1D Arrays/Floor in a Sorted Array.cpp
@@ -1,3 +1,20 @@
 question link:
 https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%253BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array
 
+class Solution {
+  public:
+    int findFloor(vector<int>& arr, int k) {
+
+        int left = 0, right = arr.size() - 1;
+        int ans = -1;
+        while(left<=right){
+            int mid = left + ((right - left) >> 1);
+            if(arr[mid] <= k){
+                ans = mid;
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+        return ans;
+    }
+};
