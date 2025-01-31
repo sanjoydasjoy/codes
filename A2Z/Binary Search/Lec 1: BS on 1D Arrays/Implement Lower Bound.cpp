690f5a66b40a2d6b03ff57c63b7daf2f25f99509
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-01-31 20:00:07 +0600

    Create Implement Lower Bound.cpp

---
+++ A2Z/Binary Search/Lec 1: BS on 1D Arrays/Implement Lower Bound.cpp
@@ -0,0 +1,19 @@
+question link:
+https://www.naukri.com/code360/problems/lower-bound_8165382?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries&leftPanelTabValue=PROBLEM
+
+
+int lowerBound(vector<int> arr, int n, int x) {
+	int left = 0, right = arr.size() - 1, mid = 0;
+	int ans = n;
+	while(left<=right){
+		int mid = left + ((right-left)>>1);
+		if(arr[mid] >= x){
+			ans = mid;
+			right = mid - 1;
+		}
+		else{
+			left = mid + 1;
+		}
+	}
+	return ans;
+}
