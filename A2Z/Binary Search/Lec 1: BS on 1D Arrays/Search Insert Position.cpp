0f5e7d6babc6a9af7f1e411a6b8aaf97dc7cd863
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-02-01 23:56:21 +0600

    Update Search Insert Position.cpp

--- A2Z/Binary Search/Lec 1: BS on 1D Arrays/Search Insert Position.cpp
+++ A2Z/Binary Search/Lec 1: BS on 1D Arrays/Search Insert Position.cpp
@@ -1,2 +1,17 @@
 question link:
 https://leetcode.com/problems/search-insert-position/description/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target)return i;
+        }
+        if(target>nums[nums.size()-1])return nums.size();
+        for(int i=0;i<nums.size()-1;i++){
+            if(target>nums[i] and target<nums[i+1])return i+1;
+        }
+        return 0;
+    
+};
+};
