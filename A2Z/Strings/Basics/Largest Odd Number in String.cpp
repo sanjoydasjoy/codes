4ce8bb507d45000ecf5c770ddd13a6df7afc3640
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-02-09 13:48:04 +0600

    Update Largest Odd Number in String.cpp

--- A2Z/Strings/Basics/Largest Odd Number in String.cpp
+++ A2Z/Strings/Basics/Largest Odd Number in String.cpp
@@ -1,2 +1,25 @@
 question link:
 https://leetcode.com/problems/largest-odd-number-in-string/description/
+
+// one problem with this problem is this has stoi function which throws error once the number is too long
+// otrhwise this is absolutely fine
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        
+        int n = num.size();
+
+        for(int i=0;i<n;i++){
+            string p = num.substr(i,n-i);
+            long long x = stoi(p);
+            if(x&1)return p;
+        }
+        for(int i=0;i<n;i++){
+            string p = num.substr(0,n-i);
+            long long x = stoi(p);
+            if(x&1)return p;
+        }
+        return "";
+    }
+};
