d4734ae160f67c370e6369c4fa808f0efb9d015c
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-02-09 13:15:20 +0600

    Update Rotate String.cpp

--- A2Z/Strings/Basics/Rotate String.cpp
+++ A2Z/Strings/Basics/Rotate String.cpp
@@ -1,1 +1,20 @@
+question link:
+https://leetcode.com/problems/rotate-string/description/
 
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s==goal)return true;
+        int n = s.size();
+        string p;
+            for(int i=0;i<n-1;i++){
+                p+=s.substr(n-i-1,i+1);
+                p+=s.substr(0,n-i-1);
+                if(p==goal)return true;
+                else p.clear();
+            }
+            return false;
+        
+    }
+};
