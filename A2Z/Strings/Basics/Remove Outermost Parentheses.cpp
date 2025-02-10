1a1565e2be33383d7af48d051bd3707fad873aa7
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-02-10 17:22:25 +0600

    Update Remove Outermost Parentheses.cpp

--- A2Z/Strings/Basics/Remove Outermost Parentheses.cpp
+++ A2Z/Strings/Basics/Remove Outermost Parentheses.cpp
@@ -1,3 +1,21 @@
 question link:
 https://leetcode.com/problems/remove-outermost-parentheses/description/
 
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int cnt = 0;
+        string ans;
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '('){
+                if(cnt > 0)ans+=s[i];
+                cnt++;
+            }
+            else if(s[i] == ')'){
+                cnt--;
+                if(cnt > 0)ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
