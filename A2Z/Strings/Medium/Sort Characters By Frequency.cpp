4c8b5b458fe3dbe4004e5dc169714eeb78dbaacf
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-02-10 17:24:17 +0600

    Update Sort Characters By Frequency.cpp

--- A2Z/Strings/Medium/Sort Characters By Frequency.cpp
+++ A2Z/Strings/Medium/Sort Characters By Frequency.cpp
@@ -1,1 +1,24 @@
+question link:
+https://leetcode.com/problems/sort-characters-by-frequency/description/
 
+class Solution {
+public:
+    string frequencySort(string s) {
+        int n = s.size();
+        map<char, int> mp;
+        for (int i = 0; i < n; i++)
+            mp[s[i]]++;
+        vector<pair<int, int>> vec(mp.begin(), mp.end());
+        sort(vec.begin(), vec.end(),
+             [](const pair<int, int>& a, const pair<int, int>& b) {
+                 return a.second < b.second;
+             });
+        string ans;
+        for(int i=vec.size()-1;i>=0;i--){
+            int x = vec[i].second;
+            for(int j=0;j<x;j++)ans+=vec[i].first;
+        }
+        return ans;     
+        
+    }
+};
