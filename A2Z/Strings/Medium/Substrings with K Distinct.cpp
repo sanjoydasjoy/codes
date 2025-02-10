79065f710c2fd86bf0913b045681c0263c688bcd
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2025-02-10 19:54:52 +0600

    Update Substrings with K Distinct.cpp

--- A2Z/Strings/Medium/Substrings with K Distinct.cpp
+++ A2Z/Strings/Medium/Substrings with K Distinct.cpp
@@ -1,1 +1,5 @@
+question link:
+https://www.geeksforgeeks.org/problems/count-number-of-substrings4528/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=count-number-of-substrings
 
+
+//
